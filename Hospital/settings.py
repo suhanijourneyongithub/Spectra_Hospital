5704c467e7ddca92b604dee20fb8cfe67ea40d58
Suhani Sharma <145221387+suhanijourneyongithub@users.noreply.github.com>
2024-10-21 13:21:43 +0530

    login and register pages done

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Hospital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ["demo_static"]
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- landing_page/urls.py
+++ landing_page/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name = "login"),
+    path("", views.register, name = "register"),
+    path("/login", views.login, name = "login"),
+    path("/forget_pass", views.forget_pass, name = "forget_pass"),
 ]
